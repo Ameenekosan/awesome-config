a1329eb38746ae73b0ca3120a43779825c9b8b39
Ameenekosan <amelie.f.lemieux@gmail.com>
2015-11-11 14:25:12 -0500

    fix wp

--- rc.lua
+++ rc.lua
@@ -89,12 +89,12 @@ local layouts =
 
 -- configuration - edit to your liking
 wp_index = 1
-wp_timeout  = 900
+wp_timeout  = 1800
 wp_path = "/home/amee/Pictures/wallpapers/"
 wp_files = { "01.jpg", "02.jpg", "03.jpg", "04.jpg" , "05.jpg" , "06.jpg" , "06.jpg" , "07.jpg" , "08.jpg" , "09.jpg" , "10.jpg" , "11.jpg" , "12.jpg" , "13.jpg" , "14.jpg" , "16.jpg" , "17.jpg" , "18.jpg" , "19.jpg" , "20.jpg" , "21.jpg" , "22.jpg" , "23.jpg" , "24.jpg" , "25.jpg" , "26.jpg" , "27.jpg" , "28.jpg" , "29.jpg" , "30.jpg" , "31.jpg" , "32.jpg" , "33.jpg" , "34.jpg", "35.jpg" , "36.jpg" , "37.jpg" , "38.jpg" , "39.jpg" , "40.jpg" , "41.jpg" , "42.jpg"   }
 
 -- setup the timer
-wp_timer = timer { timeout = wp_timeout }
+wp_timer = timer { timeout = 0 }
 wp_timer:connect_signal("timeout", function()
 
   -- set wallpaper to current index
